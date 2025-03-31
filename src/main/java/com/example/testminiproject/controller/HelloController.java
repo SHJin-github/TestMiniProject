4e9f9aeed54c571634a145404b347b985fa90e9a
jinsohee <jinthgml@gmail.com>
2025-03-31 18:02:19 +0900

    로그 설정 추가

--- src/main/java/com/example/testminiproject/controller/HelloController.java
+++ src/main/java/com/example/testminiproject/controller/HelloController.java
@@ -2,11 +2,13 @@ package com.example.testminiproject.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class HelloController {
 
-    @GetMapping("/hello")
+    @RequestMapping("/hello")
     public String hello() {
         return "hello";
     }
