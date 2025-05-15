21a69e8b6d319cd849c88ffe191fda41cd74456a
jinsohee <jinthgml@gmail.com>
2025-05-15 17:58:30 +0900

    사용자 로직 수정

--- src/main/java/com/example/testminiproject/req/vo/UserVo.java
+++ src/main/java/com/example/testminiproject/req/vo/UserVo.java
@@ -8,5 +8,6 @@ public class UserVo {
     private String userId;
     private String userName;
     private String password;
+    private String useYn;
 
 }
---
+++ src/main/java/com/example/testminiproject/util/GetUserSession.java
@@ -0,0 +1,7 @@
+package com.example.testminiproject.util;
+
+public class GetUserSession {
+
+
+
+}
